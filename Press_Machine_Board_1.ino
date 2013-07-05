7d87f8ad08a964c6bcf2e832263c36929bfecc33
Tihomir Nedev <mail@tnedev.com>
2013-07-05 16:10:48 +0300

    Press working

--- Press_Machine_Board_1.ino
+++ Press_Machine_Board_1.ino
@@ -18,57 +18,37 @@
 
 byte inPin_Presa_KraenIzklGoren = 2;
 byte inPin_Presa_KraenIzklDolen = 3;
-byte inPin_Presa_VhStrana = 4;
-byte inPin_Presa_IzhStrana = 5;
-byte inPin_Presa_VklGlavenMoror = 6;
-byte inPin_Presa_AvariaGlavenMotor = 7;
+byte inPin_Presa_AvariaGlavenMotor = 4;
 
-byte outPin_Presa_Nagore = 8;
-byte outPin_Presa_Nadolu = 9;
-byte outPin_Presa_Vakum = 10;
-byte outPin_Presa_Vazduh = 11;
+byte outPin_Presa_Nagore = 5;
+byte outPin_Presa_Nadolu = 6;
+byte outPin_Presa_Vakum = 7;
+byte outPin_Presa_Vazduh = 8;
 
 
-byte inPin_Konteiner_KrainIzklGoren = 12;
-byte inPin_Konteiner_KrainIzklDolen = 13;
-byte inPin_Konteiner_Nivo = 14;
-byte inPin_Konteiner_MotorRaboti = 15;
-byte inPin_Konteiner_MotorAvaria = 16;
-byte inPin_Konteiner_BroyachPresa = 17;
-byte inPin_Konteiner_Material = 18;
+byte inPin_Konteiner_KrainIzklDolen = 26;
+byte inPin_Konteiner_Nivo = 27;
+byte inPin_Konteiner_MotorAvaria = 28;
 
-byte outPin_Konteiner_CilindarGore = 19;
-byte outPin_Konteiner_CilindarDolu = 20;
+byte outPin_Konteiner_CilindarGore = 29;
+byte outPin_Konteiner_CilindarDolu = 30;
 
 
-byte inPin_VhMan_KraenIzklGoren = 21;
-byte inPin_VhMan_KraenIzklDolen = 22;
-byte inPin_VhMan_Reper = 23;
-byte inPin_VhMan_GornoPolozhenie = 24;
-byte inPin_VhMan_DolnoPolozhenie = 25;
-byte inPin_VhMan_Material = 26;
+byte inPin_VhMan_KraenIzklNach = 14;
+byte inPin_VhMan_KraenIzkKrai = 15;
+byte inPin_VhMan_Reper = 16;
+byte inPin_VhMan_GornoPolozhenie = 17;
+byte inPin_VhMan_DolnoPolozhenie = 20;
 
-byte outPin_VhManipulator_Gore = 27;
-byte outPin_VhManipulator_Dolu = 28;
-byte outPin_VhManipulator_Vakum = 29;
-byte outPin_VhManipulator_Vazduh = 30;
-byte outPin_VhManipulator_PosokaMotor = 31;
-byte outPin_VhManipulator_PulsMotor = 32;
+byte outPin_VhManipulator_Gore = 21;
+byte outPin_VhManipulator_Dolu = 22;
+byte outPin_VhManipulator_Vakum = 23;
+byte outPin_VhManipulator_Vazduh = 24;
+byte outPin_VhManipulator_PosokaMotor = 25;
+byte outPin_VhManipulator_PulsMotor = 45;
 
 
-byte inPin_IzhMan_KraenIzklGoren = 33;
-byte inPin_IzhMan_KraenIzklDolen = 34;
-byte inPin_IzhMan_Reper = 35;
-byte inPin_IzhMan_GornoPolozhenie = 36;
-byte inPin_IzhMan_DolnoPolozhenie = 37;
-byte inPin_IzhMan_Material = 38;
 
-byte outPin_IzhManipulator_Gore = 39;
-byte outPin_IzhManipulator_Dolu = 40;
-byte outPin_IzhManipulator_Vakum = 41;
-byte outPin_IzhManipulator_Vazduh = 42;
-byte outPin_IzhManipulator_PosokaMotor = 43;
-byte outPin_IzhManipulator_PulsMotor = 44;
 
 /*
 	Arduino main variables declaration
@@ -88,32 +68,18 @@ boolean presa_nagore = false, presa_nadolu = false; // Sets the direction of the
 int in_Presa_Pressure;
 boolean in_Presa_KraenIzklGoren = false;
 boolean in_Presa_KraenIzklDolen = false;
-boolean in_Presa_VhStrana = false;
-boolean in_Presa_IzhStrana = false;
-boolean in_Presa_VklGlavenMoror = false;
 boolean in_Presa_AvariaGlavenMotor = false;
 
-boolean in_Konteiner_KrainIzklGoren = false;
 boolean in_Konteiner_KrainIzklDolen = false;
 boolean in_Konteiner_Nivo = false;
-boolean in_Konteiner_MotorRaboti = false;
 boolean in_Konteiner_MotorAvaria = false;
-boolean in_Konteiner_BroyachPresa = false;
-boolean in_Konteiner_Material = false;
 
-boolean in_VhMan_KraenIzklGoren = false;
-boolean in_VhMan_KraenIzklDolen = false;
+boolean in_VhMan_KraenIzklNach = false;
+boolean in_VhMan_KraenIzklKrai = false;
 boolean in_VhMan_Reper = false;
 boolean in_VhMan_GornoPolozhenie = false;
 boolean in_VhMan_DolnoPolozhenie = false;
-boolean in_VhMan_Material = false;
 
-boolean in_IzhMan_KraenIzklGoren = false;
-boolean in_IzhMan_KraenIzklDolen = false;
-boolean in_IzhMan_Reper = false;
-boolean in_IzhMan_GornoPolozhenie = false;
-boolean in_IzhMan_DolnoPolozhenie = false;
-boolean in_IzhMan_Material = false;
 
 
 void setup()
@@ -124,9 +90,6 @@ void setup()
 	pinMode(A0,INPUT); // Pressure sensor
 	pinMode(inPin_Presa_KraenIzklGoren,INPUT);
 	pinMode(inPin_Presa_KraenIzklDolen,INPUT);
-	pinMode(inPin_Presa_VhStrana,INPUT);
-	pinMode(inPin_Presa_IzhStrana,INPUT);
-	pinMode(inPin_Presa_VklGlavenMoror,INPUT);
 	pinMode(inPin_Presa_AvariaGlavenMotor,INPUT);
 
 	pinMode(outPin_Presa_Nagore,OUTPUT);
@@ -134,24 +97,19 @@ void setup()
 	pinMode(outPin_Presa_Vakum,OUTPUT);
 	pinMode(outPin_Presa_Vazduh,OUTPUT);
 
-	pinMode(inPin_Konteiner_KrainIzklGoren,INPUT);
 	pinMode(inPin_Konteiner_KrainIzklDolen,INPUT);
 	pinMode(inPin_Konteiner_Nivo,INPUT);
-	pinMode(inPin_Konteiner_MotorRaboti,INPUT);
 	pinMode(inPin_Konteiner_MotorAvaria,INPUT);
-	pinMode(inPin_Konteiner_BroyachPresa,INPUT);
-	pinMode(inPin_Konteiner_Material,INPUT);
 
 	pinMode(outPin_Konteiner_CilindarGore,INPUT);
 	pinMode(outPin_Konteiner_CilindarDolu,INPUT);
 
 
-	pinMode(inPin_VhMan_KraenIzklGoren,INPUT);
-	pinMode(inPin_VhMan_KraenIzklDolen,INPUT);
+	pinMode(inPin_VhMan_KraenIzklNach,INPUT);
+	pinMode(inPin_VhMan_KraenIzkKrai,INPUT);
 	pinMode(inPin_VhMan_Reper,INPUT);
 	pinMode(inPin_VhMan_GornoPolozhenie,INPUT);
 	pinMode(inPin_VhMan_DolnoPolozhenie,INPUT);
-	pinMode(inPin_VhMan_Material,INPUT);
 
 	pinMode(outPin_VhManipulator_Gore,OUTPUT);
 	pinMode(outPin_VhManipulator_Dolu,OUTPUT);
@@ -159,21 +117,6 @@ void setup()
 	pinMode(outPin_VhManipulator_Vazduh,OUTPUT);
 	pinMode(outPin_VhManipulator_PosokaMotor,OUTPUT);
 	pinMode(outPin_VhManipulator_PulsMotor,OUTPUT);
-
-
-	pinMode(inPin_IzhMan_KraenIzklGoren,INPUT);
-	pinMode(inPin_IzhMan_KraenIzklDolen,INPUT);
-	pinMode(inPin_IzhMan_Reper,INPUT);
-	pinMode(inPin_IzhMan_GornoPolozhenie,INPUT);
-	pinMode(inPin_IzhMan_DolnoPolozhenie,INPUT);
-	pinMode(inPin_IzhMan_Material,INPUT);
-
-	pinMode(outPin_IzhManipulator_Gore,OUTPUT);
-	pinMode(outPin_IzhManipulator_Dolu,OUTPUT);
-	pinMode(outPin_IzhManipulator_Vakum,OUTPUT);
-	pinMode(outPin_IzhManipulator_Vazduh,OUTPUT);
-	pinMode(outPin_IzhManipulator_PosokaMotor,OUTPUT);
-	pinMode(outPin_IzhManipulator_PulsMotor,OUTPUT);
 
 
 }
@@ -189,32 +132,17 @@ void ReadSensors()
 	in_Presa_Pressure = analogRead(A0);
 	in_Presa_KraenIzklGoren = digitalRead(inPin_Presa_KraenIzklGoren);
 	in_Presa_KraenIzklDolen = digitalRead(inPin_Presa_KraenIzklDolen);
-	in_Presa_VhStrana = digitalRead(inPin_Presa_VhStrana);
-	in_Presa_IzhStrana = digitalRead(inPin_Presa_IzhStrana);
-	in_Presa_VklGlavenMoror = digitalRead(inPin_Presa_VklGlavenMoror);
 	in_Presa_AvariaGlavenMotor = digitalRead(inPin_Presa_AvariaGlavenMotor);
 
-	in_Konteiner_KrainIzklGoren = digitalRead(inPin_Konteiner_KrainIzklGoren);
 	in_Konteiner_KrainIzklDolen = digitalRead(inPin_Konteiner_KrainIzklDolen);
 	in_Konteiner_Nivo = digitalRead(inPin_Konteiner_Nivo);
-	in_Konteiner_MotorRaboti = digitalRead(inPin_Konteiner_MotorRaboti);
 	in_Konteiner_MotorAvaria = digitalRead(inPin_Konteiner_MotorAvaria);
-	in_Konteiner_BroyachPresa = digitalRead(inPin_Konteiner_BroyachPresa);
-	in_Konteiner_Material = digitalRead(inPin_Konteiner_Material);
 	
-	in_VhMan_KraenIzklGoren = digitalRead(inPin_VhMan_KraenIzklGoren);
-	in_VhMan_KraenIzklDolen = (inPin_VhMan_KraenIzklDolen);
+	in_VhMan_KraenIzklNach = digitalRead(inPin_VhMan_KraenIzklNach);
+	in_VhMan_KraenIzklKrai = (inPin_VhMan_KraenIzkKrai);
 	in_VhMan_Reper = digitalRead(inPin_VhMan_Reper);
 	in_VhMan_GornoPolozhenie = digitalRead(inPin_VhMan_GornoPolozhenie);
 	in_VhMan_DolnoPolozhenie = digitalRead(inPin_VhMan_DolnoPolozhenie);
-	in_VhMan_Material = digitalRead(inPin_VhMan_Material);
-
-	in_IzhMan_KraenIzklGoren = digitalRead(inPin_IzhMan_KraenIzklGoren);
-	in_IzhMan_KraenIzklDolen = digitalRead(inPin_IzhMan_KraenIzklDolen);
-	in_IzhMan_Reper = digitalRead(inPin_IzhMan_Reper);
-	in_IzhMan_GornoPolozhenie = digitalRead(inPin_IzhMan_GornoPolozhenie);
-	in_IzhMan_DolnoPolozhenie = digitalRead(inPin_IzhMan_DolnoPolozhenie);
-	in_IzhMan_Material = digitalRead(inPin_IzhMan_Material);
 
 
 }
@@ -226,61 +154,63 @@ void Press()
 {
 
 	if(in_Presa_AvariaGlavenMotor==LOW) // Is there an emergency in the press
-	{
-		if (in_Presa_VklGlavenMoror) // Is the main motor of the press functioning
-		// There is no emergency and the motor is working, start the normal operation of the press
+	{ // Main function of the press
+		if(in_Presa_KraenIzklGoren == false && in_Presa_KraenIzklDolen == true) // If the press is at up position start going down
 		{
-			if(	in_Presa_VhStrana == LOW && in_Presa_IzhStrana == LOW) // Check if manipulators are on the way of the press
-			{
-				if(in_Presa_KraenIzklGoren==true && in_Presa_KraenIzklDolen==false)
-				{
-					presa_nadolu = true; // the press should move down
-					presa_nagore = false;
-				}
-				else if(in_Presa_KraenIzklGoren==false && in_Presa_KraenIzklDolen==true && in_Presa_Pressure>=pressureThreshold)
-				{
-					presa_nagore = true; // the press should move up
-					presa_nadolu = false; 
-				}
-				else if (presa_nagore == false && presa_nadolu == false) // if the program has started but the press is somewhere in between, start the press down
-				{
-					presa_nadolu = true;
-				}
-				
-				if(presa_nadolu) // When the press should go down
-				{
-					digitalWrite(outPin_Presa_Nagore, LOW); // Stop the press going up
-					digitalWrite(outPin_Presa_Vazduh, LOW); // Turn off the air
-					
-					digitalWrite(outPin_Presa_Nadolu, HIGH); // Start the press going down
-					digitalWrite(outPin_Presa_Vakum, HIGH); // Start the vacum
-				}
-				else if(presa_nagore) // When the press should go up
-				{
-					digitalWrite(outPin_Presa_Nadolu, LOW); // Stop the press going down
-					digitalWrite(outPin_Presa_Vakum, LOW); // Stop the vacum
-					
-					digitalWrite(outPin_Presa_Nagore, HIGH); // Start the press going up
-					digitalWrite(outPin_Presa_Vazduh, HIGH); // Start the air
-				}
-			}
+			presa_nadolu = 1;
+			presa_nagore = 0;
+		}
+		else if(in_Presa_KraenIzklGoren == true && in_Presa_KraenIzklDolen == false && in_Presa_Pressure >= pressureThreshold) // If the press is down start going up
+		{
+                        presa_nagore = 1;
+			presa_nadolu = 0;
 			
-			else // Manupulators are on the way of the press, stop the press, stop the vacum and the air
-			{
-				digitalWrite(outPin_Presa_Nagore, LOW);
-				digitalWrite(outPin_Presa_Nadolu, LOW);
-				digitalWrite(outPin_Presa_Vazduh, LOW);
-				digitalWrite(outPin_Presa_Vakum, LOW);
-			}
 		}
-		else
+
+		else if (presa_nagore==false && presa_nadolu==false )
 		{
-			Serial.write("The main motor of the press is not working");
+			presa_nagore = true;
+                        
 		}
+
+		if(presa_nadolu==true && presa_nagore==false)// Start moving the press down
+		{
+			digitalWrite(outPin_Presa_Nagore, LOW);
+			digitalWrite(outPin_Presa_Nadolu, HIGH);
+		}
+		 else if(presa_nadolu==false && presa_nagore==true) // Start moving the press up
+		{
+			digitalWrite(outPin_Presa_Nadolu, LOW);
+			digitalWrite(outPin_Presa_Nagore, HIGH);
+		}
+		else 
+		{
+			Serial.println("Presa_Nagore and Presa_Nadolu are not correct");
+		}
+
+
 	}
 	else
 	{
-		Serial.write("Emergency stop at the the press");
+		Serial.println("Emergency stop at the the press");
+		emergency = HIGH;
+	}
+
+}
+
+void Konteiner()
+/*
+	This function operates the press module on the production line
+*/
+{
+
+	if(in_Konteiner_MotorAvaria==LOW) // Is there an emergency in the container
+	{ 
+		
+	}
+	else
+	{
+		Serial.write("Emergency stop at the the container");
 		emergency = HIGH;
 	}
 
@@ -291,8 +221,13 @@ void ReadEmergency()
 	This function will block the operation of the production line while
 	an emergency sensors is activated. The program reads all emergency sensors.
 */
-{
-	if(in_Presa_AvariaGlavenMotor==LOW)
+{	
+	ReadSensors();
+	if(in_Presa_AvariaGlavenMotor == LOW &&
+		in_Konteiner_MotorAvaria == LOW
+		
+	
+	)
 	{
 		emergency = LOW;
 	}
--- Press_Machine_Board_2.ino
+++ Press_Machine_Board_2.ino
@@ -15,53 +15,67 @@
 	names formation - "inPin"/"outPin"+Function_name+ name
 	The values links the pin number with the name
 */
+byte inPin_IzhMan_KraenIzklNach = 38;
+byte inPin_IzhMan_KraenIzklKrai = 39;
+byte inPin_IzhMan_Reper = 40;
+byte inPin_IzhMan_GornoPolozhenie = 41;
+byte inPin_IzhMan_DolnoPolozhenie = 42;
 
-byte inPin_Razmotalka_DatFolio = 45;
-byte inPin_Razmotalka_SenzorFolio = 46;
-byte inPin_Razmotalka_DatAkumFolio = 47;
-byte inPin_Razmotalka_DatIzkonsumiranAkum = 48;
-byte inPin_Razmotalka_DatKraiFolio = 49;
-byte inPin_Razmotalka_NozhStart = 50;
-byte inPin_Razmotalka_NozhStop = 51;
-byte inPin_Razmotalka_MotorVkl = 52;
-byte inPin_Razmotalka_MotorAvaria = 53;
+byte outPin_IzhManipulator_Gore = 43;
+byte outPin_IzhManipulator_Dolu = 48;
+byte outPin_IzhManipulator_Vakum = 45;
+byte outPin_IzhManipulator_Vazduh = 46;
+byte outPin_IzhManipulator_PosokaMotor = 47;
+byte outPin_IzhManipulator_PulsMotor = 45;
 
-byte outPin_Razmotalka_GlavenMotor = 54;
-byte outPin_Razmotalka_MotorVisSkorost = 55;
-byte outPin_Razmotalka_MotorNisSkorost = 56;
-byte outPin_Razmotalka_NozhicaOtiva = 57;
-byte outPin_Razmotalka_NozhicaVrashta = 58;
-byte outPin_Razmotalka_StartNozhicaDisk = 59;
-byte outPin_Razmotalka_Vakum = 60;
+byte inPin_Razmotalka_SenzorFolio1 = 14;
+byte inPin_Razmotalka_SenzorFolio2 = 15;
+byte inPin_Razmotalka_DatAkumFolio = 16;
+byte inPin_Razmotalka_DatIzkonsumiranAkum = 17;
+byte inPin_Razmotalka_DatKraiFolio = 18;
+byte inPin_Razmotalka_NozhStart = 19;
+byte inPin_Razmotalka_NozhStop = 20;
+byte inPin_Razmotalka_MotorAvaria = 21;
 
-byte inPin_Nozhica_List = 61;
-byte inPin_Nozhica_GornoPolozh = 62;
-byte inPin_Nozhica_DolnoPolozh = 63;
+byte outPin_Razmotalka_GlavenMotor = 22;
+byte outPin_Razmotalka_MotorVisSkorost = 23;
+byte outPin_Razmotalka_MotorNisSkorost = 24;
+byte outPin_Razmotalka_NozhicaOtiva = 25;
+byte outPin_Razmotalka_NozhicaVrashta = 26;
+byte outPin_Razmotalka_StartNozhicaDisk = 27;
+byte outPin_Razmotalka_Vakum = 28;
 
-byte outPin_Nozhica_MasaGore = 64;
-byte outPin_Nozhica_Nozhici1Napred = 65;
-byte outPin_Nozhica_Nozhici2Napred = 66;
-byte outPin_Nozhica_Nozhici1Nazad = 67;
-byte outPin_Nozhica_Nozhici2Nazad = 68;
-byte outPin_Nozhica_Motori1Disk = 69;
-byte outPin_Nozhica_Motori2Disk = 70;
+byte inPin_Nozhica_GornoPolozh = 29;
+byte inPin_Nozhica_DolnoPolozh = 30;
+
+byte outPin_Nozhica_MasaGore = 31;
+byte outPin_Nozhica_Nozhici1Napred = 32;
+byte outPin_Nozhica_Nozhici2Napred = 34;
+byte outPin_Nozhica_Nozhici1Nazad = 33;
+byte outPin_Nozhica_Nozhici2Nazad = 35;
+byte outPin_Nozhica_Motori1Disk =  36;
+byte outPin_Nozhica_Motori2Disk = 37;
 
 /*
 	Arduino variables declaration
 	names formation - "input"/"output" + Function_name + name
 */
+boolean in_IzhMan_KraenIzklNach = false;
+boolean in_IzhMan_KraenIzklKrai = false;
+boolean in_IzhMan_Reper = false;
+boolean in_IzhMan_GornoPolozhenie = false;
+boolean in_IzhMan_DolnoPolozhenie = false;
+boolean in_IzhMan_Material = false;
 
-boolean in_Razmotalka_DatFolio = false;
-boolean in_Razmotalka_SenzorFolio = false;
+boolean in_Razmotalka_SenzorFolio1 = false;
+boolean in_Razmotalka_SenzorFolio2 = false;
 boolean in_Razmotalka_DatAkumFolio = false;
 boolean in_Razmotalka_DatIzkonsumiranAkum = false;
 boolean in_Razmotalka_DatKraiFolio = false;
 boolean in_Razmotalka_NozhStart = false;
 boolean in_Razmotalka_NozhStop = false;
-boolean in_Razmotalka_MotorVkl = false;
 boolean in_Razmotalka_MotorAvaria = false;
 
-boolean in_Nozhica_List = false;
 boolean in_Nozhica_GornoPolozh = false;
 boolean in_Nozhica_DolnoPolozh = false;
 
@@ -70,14 +84,29 @@ void setup()
 	Serial.begin(9600);
 
   // Arduino pinmodes set up
-	pinMode(inPin_Razmotalka_DatFolio,INPUT);
-	pinMode(inPin_Razmotalka_SenzorFolio,INPUT);
+  
+
+	pinMode(inPin_IzhMan_KraenIzklNach,INPUT);
+	pinMode(inPin_IzhMan_KraenIzklKrai,INPUT);
+	pinMode(inPin_IzhMan_Reper,INPUT);
+	pinMode(inPin_IzhMan_GornoPolozhenie,INPUT);
+	pinMode(inPin_IzhMan_DolnoPolozhenie,INPUT);
+
+
+	pinMode(outPin_IzhManipulator_Gore,OUTPUT);
+	pinMode(outPin_IzhManipulator_Dolu,OUTPUT);
+	pinMode(outPin_IzhManipulator_Vakum,OUTPUT);
+	pinMode(outPin_IzhManipulator_Vazduh,OUTPUT);
+	pinMode(outPin_IzhManipulator_PosokaMotor,OUTPUT);
+	pinMode(outPin_IzhManipulator_PulsMotor,OUTPUT);
+  
+	pinMode(inPin_Razmotalka_SenzorFolio1,INPUT);
+	pinMode(inPin_Razmotalka_SenzorFolio2,INPUT);
 	pinMode(inPin_Razmotalka_DatAkumFolio,INPUT);
 	pinMode(inPin_Razmotalka_DatIzkonsumiranAkum,INPUT);
 	pinMode(inPin_Razmotalka_DatKraiFolio,INPUT);
 	pinMode(inPin_Razmotalka_NozhStart,INPUT);
 	pinMode(inPin_Razmotalka_NozhStop,INPUT);
-	pinMode(inPin_Razmotalka_MotorVkl,INPUT);
 	pinMode(inPin_Razmotalka_MotorAvaria,INPUT);
 
 	pinMode(outPin_Razmotalka_GlavenMotor,OUTPUT);
@@ -88,7 +117,6 @@ void setup()
 	pinMode(outPin_Razmotalka_StartNozhicaDisk,OUTPUT);
 	pinMode(outPin_Razmotalka_Vakum,OUTPUT);
 
-	pinMode(inPin_Nozhica_List,INPUT);
 	pinMode(inPin_Nozhica_GornoPolozh,INPUT);
 	pinMode(inPin_Nozhica_DolnoPolozh,INPUT);
 
@@ -108,17 +136,21 @@ void ReadSensors()
 	reading the sensors only in one function
 */
 {
-	in_Razmotalka_DatFolio = digitalRead(inPin_Razmotalka_DatFolio);
-	in_Razmotalka_SenzorFolio  = digitalRead(inPin_Razmotalka_SenzorFolio);
+	in_IzhMan_KraenIzklNach = digitalRead(inPin_IzhMan_KraenIzklNach);
+	in_IzhMan_KraenIzklKrai = digitalRead(inPin_IzhMan_KraenIzklKrai);
+	in_IzhMan_Reper = digitalRead(inPin_IzhMan_Reper);
+	in_IzhMan_GornoPolozhenie = digitalRead(inPin_IzhMan_GornoPolozhenie);
+	in_IzhMan_DolnoPolozhenie = digitalRead(inPin_IzhMan_DolnoPolozhenie);
+
+	in_Razmotalka_SenzirFolio1 = digitalRead(inPin_Razmotalka_SenzorFolio1);
+	in_Razmotalka_SenzorFolio2  = digitalRead(inPin_Razmotalka_SenzorFolio2);
 	in_Razmotalka_DatAkumFolio  = digitalRead(inPin_Razmotalka_DatAkumFolio);
 	in_Razmotalka_DatIzkonsumiranAkum  = digitalRead(inPin_Razmotalka_DatIzkonsumiranAkum);
 	in_Razmotalka_DatKraiFolio  = digitalRead(inPin_Razmotalka_DatKraiFolio);
 	in_Razmotalka_NozhStart  = digitalRead(inPin_Razmotalka_NozhStart);
 	in_Razmotalka_NozhStop  = digitalRead(inPin_Razmotalka_NozhStop);
-	in_Razmotalka_MotorVkl  = digitalRead(inPin_Razmotalka_MotorVkl);
 	in_Razmotalka_MotorAvaria  = digitalRead(inPin_Razmotalka_MotorAvaria);
 
-	in_Nozhica_List  = digitalRead(inPin_Nozhica_List);
 	in_Nozhica_GornoPolozh  = digitalRead(inPin_Nozhica_GornoPolozh);
 	in_Nozhica_DolnoPolozh  = digitalRead(inPin_Nozhica_DolnoPolozh);
 
