ffd3054f4b7c129bd515b1e205d80c58307c0b0a
Tihomir Nedev <mail@tnedev.com>
2013-07-11 14:48:48 +0300

    New more secure input function added. Both manipulators are installed.

--- Press_Machine_Board_1.ino
+++ Press_Machine_Board_1.ino
@@ -11,13 +11,20 @@
 */
 
 /*
+	Progam, how to add new inputs?
+	If a new input has to be added to the program the following actions should be done. 
+	1. Under pins set up - add name and number for the pin.
+	2. Declare a boolean/int value assigned to the new input.
+	3. Add the pinMode for the input under Setup().
+	4. Add the variable for this input and its declared pin under ReadSensors() function.
+*/
+
+
+/*
 	Arduino pins set up
 	names formation - "inPin"/"outPin"+Function_name+ name
 	The values links the pin number with the name
 */
-
-
-
 
 byte inPin_Presa_KraenIzklGoren = 23;
 byte inPin_Presa_KraenIzklDolen = 25;
@@ -170,14 +177,15 @@ void setup()
 	pinMode(outPin_Nozhica_Butalo2,OUTPUT);
 	pinMode(outPin_Nozhica_EnableNozh,OUTPUT);
 	
-        digitalWrite(inPin_Presa_KraenIzklGoren, HIGH);
-        digitalWrite(inPin_Presa_KraenIzklDolen, HIGH);
-        digitalWrite(inPin_Presa_AvariaGlavenMotor, HIGH);
-        digitalWrite(inPin_VhMan_KraenIzklNach, HIGH);
-        digitalWrite(inPin_VhMan_KraenIzklKrai, HIGH);
-        digitalWrite(inPin_VhMan_ReperNach, HIGH);
-        digitalWrite(inPin_VhMan_ReperKrai, HIGH);
-        digitalWrite(27, HIGH);
+	// Pull-up resistor for the inverted inputs
+	digitalWrite(inPin_Presa_KraenIzklGoren, HIGH);
+	digitalWrite(inPin_Presa_KraenIzklDolen, HIGH);
+	digitalWrite(inPin_Presa_AvariaGlavenMotor, HIGH);
+	digitalWrite(inPin_VhMan_KraenIzklNach, HIGH);
+	digitalWrite(inPin_VhMan_KraenIzklKrai, HIGH);
+	digitalWrite(inPin_VhMan_ReperNach, HIGH);
+	digitalWrite(inPin_VhMan_ReperKrai, HIGH);
+	digitalWrite(27, HIGH);
 
 
 
@@ -187,33 +195,139 @@ void ReadSensors()
 /*
 	This function reads all the sensors the funcions on this board. 
 	Used as a convinient way to read and store all values instead of 
-	reading the sensors only in one function
+	reading the sensors only in one function. Using global values is helpful as it will save time in execution and
+	readings will be available all the time. 
+	This function takes 5 readings in between 4 microseconds and only if they are all the same, it stores the new value. 
+	The analog is taken by reading 50 values and taking an avarage. 
 */
 
 {
-	in_Presa_Pressure = analogRead(A0);
-	in_Presa_KraenIzklGoren = digitalRead(inPin_Presa_KraenIzklGoren);
-	in_Presa_KraenIzklDolen = digitalRead(inPin_Presa_KraenIzklDolen);
-	in_Presa_AvariaGlavenMotor = digitalRead(inPin_Presa_AvariaGlavenMotor);
+	// Bolean values to be used to store 5 readings
+	boolean digital_1[5];
+	boolean digital_2[5];
+	boolean digital_3[5];
+	boolean digital_4[5];
+	boolean digital_5[5];
+	boolean digital_6[5];
+	boolean digital_7[5];
+	boolean digital_8[5];
+	boolean digital_9[5];
+	boolean digital_10[5];
+	boolean digital_11[5];
+	boolean digital_12[5];
+	boolean digital_13[5];
+	boolean digital_14[5];
+	boolean digital_15[5];
+	boolean digital_16[5];
+	boolean digital_17[5];
+	boolean digital_18[5];
+	int analog_1[50];
+	unsigned long analog_sum=0;
 
-	in_Konteiner_KrainIzklDolen = digitalRead(inPin_Konteiner_KrainIzklDolen);
-	in_Konteiner_Nivo = digitalRead(inPin_Konteiner_Nivo);
-	in_Konteiner_MotorAvaria = digitalRead(inPin_Konteiner_MotorAvaria);
-	
-	in_VhMan_KraenIzklNach = digitalRead(inPin_VhMan_KraenIzklNach);
-	in_VhMan_KraenIzklKrai = digitalRead(inPin_VhMan_KraenIzklKrai);
-	in_VhMan_ReperNach = digitalRead(inPin_VhMan_ReperNach);
-	in_VhMan_ReperKrai = digitalRead(inPin_VhMan_ReperKrai);
-	in_VhMan_GornoPolozhenie = digitalRead(inPin_VhMan_GornoPolozhenie);
-	in_VhMan_DolnoPolozhenie = digitalRead(inPin_VhMan_DolnoPolozhenie);
-	
-	in_Nozhica_GornoPolozh = digitalRead(inPin_Nozhica_GornoPolozh);
-	in_Nozhica_DolnoPolozh = digitalRead(inPin_Nozhica_DolnoPolozh);
-	in_Nozhica_Ready1_1 = digitalRead(inPin_Nozhica_Ready1_1);
-	in_Nozhica_Ready1_2 = digitalRead(inPin_Nozhica_Ready1_2);
-	in_Nozhica_Ready2_1 = digitalRead(inPin_Nozhica_Ready2_1);
-	in_Nozhica_Ready2_2 = digitalRead(inPin_Nozhica_Ready2_2);
-	in_Nozhica_Ready2_2 = digitalRead(inPin_Nozhica_Ready2_2);
+	for (int k=0; k<5;k++) // Take 5 readings for all inputs
+	{
+		digital_1[k] = digitalRead(inPin_Presa_KraenIzklGoren);
+		digital_2[k] = digitalRead(inPin_Presa_KraenIzklDolen);
+		digital_3[k] = digitalRead(inPin_Presa_AvariaGlavenMotor);
+		digital_4[k] = digitalRead(inPin_Konteiner_KrainIzklDolen);
+		digital_5[k] = digitalRead(inPin_Konteiner_Nivo);
+		digital_6[k] = digitalRead(inPin_Konteiner_MotorAvaria);
+		digital_7[k] = digitalRead(inPin_VhMan_KraenIzklNach);
+		digital_8[k] = digitalRead(inPin_VhMan_KraenIzklKrai);
+		digital_9[k] = digitalRead(inPin_VhMan_ReperNach);
+		digital_10[k] = digitalRead(inPin_VhMan_ReperKrai);
+		digital_11[k] = digitalRead(inPin_VhMan_GornoPolozhenie);
+		digital_12[k] = digitalRead(inPin_VhMan_DolnoPolozhenie);
+		digital_13[k] = digitalRead(inPin_Nozhica_GornoPolozh);
+		digital_14[k] = digitalRead(inPin_Nozhica_DolnoPolozh);
+		digital_15[k] = digitalRead(inPin_Nozhica_Ready1_1);
+		digital_16[k] = digitalRead(inPin_Nozhica_Ready1_2);
+		digital_17[k] = digitalRead(inPin_Nozhica_Ready2_1);
+		digital_18[k] = digitalRead(inPin_Nozhica_Ready2_2);
+		
+		
+		delayMicroseconds(4);
+	}
+	// Check if all 5 readings are the same. If they are the same, store the value. If not, go to the next value. 	
+	if(digital_1[0]==digital_1[1] && digital_1[0]==digital_1[2]&& digital_1[0]==digital_1[3]&& digital_1[0]==digital_1[4])
+	{
+		in_Presa_KraenIzklGoren = digital_1[0];
+	}
+	if(digital_2[0]==digital_2[1] && digital_2[0]==digital_2[2]&& digital_2[0]==digital_2[3]&& digital_2[0]==digital_2[4])
+	{
+		in_Presa_KraenIzklDolen = digital_2[0];
+	}
+	if(digital_3[0]==digital_3[1] && digital_3[0]==digital_3[2]&& digital_3[0]==digital_3[3]&& digital_3[0]==digital_3[4])
+	{
+		in_Presa_AvariaGlavenMotor = digital_3[0];
+	}
+	if(digital_4[0]==digital_4[1] && digital_4[0]==digital_4[2]&& digital_4[0]==digital_4[3]&& digital_4[0]==digital_4[4])
+	{
+		in_Konteiner_KrainIzklDolen = digital_4[0];
+	}
+	if(digital_5[0]==digital_5[1] && digital_5[0]==digital_5[2]&& digital_5[0]==digital_5[3]&& digital_5[0]==digital_5[4])
+	{
+		in_Konteiner_Nivo = digital_5[0];
+	}
+	if(digital_6[0]==digital_6[1] && digital_6[0]==digital_6[2]&& digital_6[0]==digital_6[3]&& digital_6[0]==digital_6[4])
+	{
+		in_Konteiner_MotorAvaria = digital_6[0];
+	}
+	if(digital_7[0]==digital_7[1] && digital_7[0]==digital_7[2]&& digital_7[0]==digital_7[3]&& digital_7[0]==digital_7[4])
+	{
+		in_VhMan_KraenIzklNach = digital_7[0];
+	}
+	if(digital_8[0]==digital_8[1] && digital_8[0]==digital_8[2]&& digital_8[0]==digital_8[3]&& digital_8[0]==digital_8[4])
+	{
+		in_VhMan_KraenIzklKrai = digital_8[0];
+	}
+	if(digital_9[0]==digital_9[1] && digital_9[0]==digital_9[2]&& digital_9[0]==digital_9[3]&& digital_9[0]==digital_9[4])
+	{
+		in_VhMan_ReperNach = digital_9[0];
+	}
+	if(digital_10[0]==digital_10[1] && digital_10[0]==digital_10[2]&& digital_10[0]==digital_10[3]&& digital_10[0]==digital_10[4])
+	{
+		in_VhMan_ReperKrai = digital_10[0];
+	}
+	if(digital_10[0]==digital_11[1] && digital_11[0]==digital_11[2]&& digital_11[0]==digital_11[3]&& digital_11[0]==digital_11[4])
+	{
+		in_VhMan_GornoPolozhenie = digital_11[0];
+	}
+	if(digital_12[0]==digital_12[1] && digital_12[0]==digital_12[2]&& digital_12[0]==digital_12[3]&& digital_12[0]==digital_12[4])
+	{
+		in_VhMan_DolnoPolozhenie = digital_12[0];
+	}
+	if(digital_13[0]==digital_13[1] && digital_13[0]==digital_13[2]&& digital_13[0]==digital_13[3]&& digital_13[0]==digital_13[4])
+	{
+		in_Nozhica_GornoPolozh = digital_13[0];
+	}
+	if(digital_14[0]==digital_14[1] && digital_14[0]==digital_14[2]&& digital_14[0]==digital_14[3]&& digital_14[0]==digital_14[4])
+	{
+		in_Nozhica_DolnoPolozh = digital_14[0];
+	}
+	if(digital_15[0]==digital_15[1] && digital_15[0]==digital_15[2]&& digital_15[0]==digital_15[3]&& digital_15[0]==digital_15[4])
+	{
+		in_Nozhica_Ready1_1 = digital_15[0];
+	}
+	if(digital_16[0]==digital_16[1] && digital_16[0]==digital_16[2]&& digital_16[0]==digital_16[3]&& digital_16[0]==digital_16[4])
+	{
+		in_Nozhica_Ready1_2 = digital_16[0];
+	}
+	if(digital_17[0]==digital_17[1] && digital_17[0]==digital_17[2]&& digital_17[0]==digital_17[3]&& digital_17[0]==digital_17[4])
+	{
+		in_Nozhica_Ready2_1 = digital_17[0];
+	}
+	if(digital_18[0]==digital_18[1] && digital_18[0]==digital_18[2]&& digital_18[0]==digital_18[3]&& digital_18[0]==digital_18[4])
+	{
+		in_Nozhica_Ready2_2 = digital_18[0];
+	}
+
+	for (int k=0; k<50;k++)
+	{
+		analog_sum = analog_sum+analogRead(A0);
+	}
+	in_Presa_Pressure = analog_sum/50;
+
 	
 
 }
@@ -270,14 +384,11 @@ void Press()
 	}
 
 }
-
 void VhManipulator()
 /*
 	This function operates the input manipulator of the system
 */
 {
-	
-	
 	
 	if( ( in_VhMan_ReperNach && current_speed == 0) ||(in_VhMan_ReperKrai && current_speed == 0))
 	{	// When the end switch and reper sensor are HIGH and the speed of the motor is 0, this calls for acceleration of the motor
@@ -422,10 +533,10 @@ else
   
 
 /*
-  Serial.print(in_IzhMan_ReperNach);
-  Serial.print(in_IzhMan_KraenIzklNach);
-  Serial.print(in_IzhMan_KraenIzklKrai);
-  Serial.print(in_IzhMan_ReperKrai);
+  Serial.print(in_VhMan_ReperNach);
+  Serial.print(in_VhMan_KraenIzklNach);
+  Serial.print(in_VhMan_KraenIzklKrai);
+  Serial.print(in_VhMan_ReperKrai);
   
   Serial.print(in_VhMan_ReperNach);
   Serial.print(in_VhMan_KraenIzklNach);
--- Press_Machine_Board_2.ino
+++ Press_Machine_Board_2.ino
@@ -10,13 +10,19 @@
 */
 
 /*
+	Progam, how to add new inputs?
+	If a new input has to be added to the program the following actions should be done. 
+	1. Under pins set up - add name and number for the pin.
+	2. Declare a boolean/int value assigned to the new input.
+	3. Add the pinMode for the input under Setup().
+	4. Add the variable for this input and its declared pin under ReadSensors() function.
+*/
+
+/*
 	Arduino pins set up
 	names formation - "inPin"/"outPin"+Function_name+ name
 	The values links the pin number with the name
 */
-
-
-
 
 
 byte inPin_IzhMan_KraenIzklNach = 24;
@@ -153,26 +159,123 @@ void ReadSensors()
 /*
 	This function reads all the sensors the funcions on this board. 
 	Used as a convinient way to read and store all values instead of 
-	reading the sensors only in one function
+	reading the sensors only in one function. Using global values is helpful as it will save time in execution and
+	readings will be available all the time. 
+	This function takes 5 readings in between 4 microseconds and only if they are all the same, it stores the new value. 
+	The analog is taken by reading 50 values and taking an avarage. 
 */
-{
-	in_IzhMan_KraenIzklNach = digitalRead(inPin_IzhMan_KraenIzklNach);
-	in_IzhMan_KraenIzklKrai = digitalRead(inPin_IzhMan_KraenIzklKrai);
-	in_IzhMan_ReperNach = digitalRead(inPin_IzhMan_ReperNach);
-	in_IzhMan_ReperKrai = digitalRead(inPin_IzhMan_ReperKrai);
-	in_IzhMan_GornoPolozhenie = digitalRead(inPin_IzhMan_GornoPolozhenie);
-	in_IzhMan_DolnoPolozhenie = digitalRead(inPin_IzhMan_DolnoPolozhenie);
 
-	in_Razmotalka_SenzorFolio1 = digitalRead(inPin_Razmotalka_SenzorFolio1);
-	in_Razmotalka_SenzorFolio2 = digitalRead(inPin_Razmotalka_SenzorFolio2);
-	in_Razmotalka_DatAkumFolio = digitalRead(inPin_Razmotalka_DatAkumFolio);
-	in_Razmotalka_DatIzkonsumiranAkum = digitalRead(inPin_Razmotalka_DatIzkonsumiranAkum);
-	in_Razmotalka_DatKraiFolio = digitalRead(inPin_Razmotalka_DatKraiFolio);
-	in_Razmotalka_NozhStart = digitalRead(inPin_Razmotalka_NozhStart);
-	in_Razmotalka_NozhStop = digitalRead(inPin_Razmotalka_NozhStop);
-	in_Razmotalka_ReadyServo = digitalRead(inPin_Razmotalka_ReadyServo);
-	in_Razmotalka_ReadyNozh = digitalRead(inPin_Razmotalka_ReadyNozh);
-	in_Razmotalka_ReadyDisk = digitalRead(inPin_Razmotalka_ReadyDisk);
+{
+	// Bolean values to be used to store 5 readings
+	boolean digital_1[5];
+	boolean digital_2[5];
+	boolean digital_3[5];
+	boolean digital_4[5];
+	boolean digital_5[5];
+	boolean digital_6[5];
+	boolean digital_7[5];
+	boolean digital_8[5];
+	boolean digital_9[5];
+	boolean digital_10[5];
+	boolean digital_11[5];
+	boolean digital_12[5];
+	boolean digital_13[5];
+	boolean digital_14[5];
+	boolean digital_15[5];
+	boolean digital_16[5];
+
+
+	for (int k=0; k<5;k++) // Take 5 readings for all inputs
+	{
+		digital_1[k] = digitalRead(inPin_IzhMan_KraenIzklNach);
+		digital_2[k] = digitalRead(inPin_IzhMan_KraenIzklKrai);
+		digital_3[k] = digitalRead(inPin_IzhMan_ReperNach);
+		digital_4[k] = digitalRead(inPin_IzhMan_ReperKrai);
+		digital_5[k] = digitalRead(inPin_IzhMan_GornoPolozhenie);
+		digital_6[k] = digitalRead(inPin_IzhMan_DolnoPolozhenie);
+		
+		digital_7[k] = digitalRead(inPin_Razmotalka_SenzorFolio1);
+		digital_8[k] = digitalRead(inPin_Razmotalka_SenzorFolio2);
+		digital_9[k] = digitalRead(inPin_Razmotalka_DatAkumFolio);
+		digital_10[k] = digitalRead(inPin_Razmotalka_DatIzkonsumiranAkum);
+		digital_11[k] = digitalRead(inPin_Razmotalka_DatKraiFolio);
+		digital_12[k] = digitalRead(inPin_Razmotalka_NozhStart);
+		digital_13[k] = digitalRead(inPin_Razmotalka_NozhStop);
+		digital_14[k] = digitalRead(inPin_Razmotalka_ReadyServo);
+		digital_15[k] = digitalRead(inPin_Razmotalka_ReadyNozh);
+		digital_16[k] = digitalRead(inPin_Razmotalka_ReadyDisk);
+
+				
+		delayMicroseconds(4);
+	}
+
+	// Check if all 5 readings are the same. If they are the same, store the value. If not, go to the next value. 
+	
+	if(digital_1[0]==digital_1[1] && digital_1[0]==digital_1[2]&& digital_1[0]==digital_1[3]&& digital_1[0]==digital_1[4])
+	{
+		in_IzhMan_KraenIzklNach = digital_1[0];
+	}
+	if(digital_2[0]==digital_2[1] && digital_2[0]==digital_2[2]&& digital_2[0]==digital_2[3]&& digital_2[0]==digital_2[4])
+	{
+		in_IzhMan_KraenIzklKrai = digital_2[0];
+	}
+	if(digital_3[0]==digital_3[1] && digital_3[0]==digital_3[2]&& digital_3[0]==digital_3[3]&& digital_3[0]==digital_3[4])
+	{
+		in_IzhMan_ReperNach = digital_3[0];
+	}
+	if(digital_4[0]==digital_4[1] && digital_4[0]==digital_4[2]&& digital_4[0]==digital_4[3]&& digital_4[0]==digital_4[4])
+	{
+		in_IzhMan_ReperKrai = digital_4[0];
+	}
+	if(digital_5[0]==digital_5[1] && digital_5[0]==digital_5[2]&& digital_5[0]==digital_5[3]&& digital_5[0]==digital_5[4])
+	{
+		in_IzhMan_GornoPolozhenie = digital_5[0];
+	}
+	if(digital_6[0]==digital_6[1] && digital_6[0]==digital_6[2]&& digital_6[0]==digital_6[3]&& digital_6[0]==digital_6[4])
+	{
+		in_IzhMan_DolnoPolozhenie = digital_6[0];
+	}
+	if(digital_7[0]==digital_7[1] && digital_7[0]==digital_7[2]&& digital_7[0]==digital_7[3]&& digital_7[0]==digital_7[4])
+	{
+		in_Razmotalka_SenzorFolio1 = digital_7[0];
+	}
+	if(digital_8[0]==digital_8[1] && digital_8[0]==digital_8[2]&& digital_8[0]==digital_8[3]&& digital_8[0]==digital_8[4])
+	{
+		in_Razmotalka_SenzorFolio2 = digital_8[0];
+	}
+	if(digital_9[0]==digital_9[1] && digital_9[0]==digital_9[2]&& digital_9[0]==digital_9[3]&& digital_9[0]==digital_9[4])
+	{
+		in_Razmotalka_DatAkumFolio = digital_9[0];
+	}
+	if(digital_10[0]==digital_10[1] && digital_10[0]==digital_10[2]&& digital_10[0]==digital_10[3]&& digital_10[0]==digital_10[4])
+	{
+		in_Razmotalka_DatIzkonsumiranAkum = digital_10[0];
+	}
+	if(digital_10[0]==digital_11[1] && digital_11[0]==digital_11[2]&& digital_11[0]==digital_11[3]&& digital_11[0]==digital_11[4])
+	{
+		in_Razmotalka_DatKraiFolio = digital_11[0];
+	}
+	if(digital_12[0]==digital_12[1] && digital_12[0]==digital_12[2]&& digital_12[0]==digital_12[3]&& digital_12[0]==digital_12[4])
+	{
+		in_Razmotalka_NozhStart = digital_12[0];
+	}
+	if(digital_13[0]==digital_13[1] && digital_13[0]==digital_13[2]&& digital_13[0]==digital_13[3]&& digital_13[0]==digital_13[4])
+	{
+		in_Razmotalka_NozhStop = digital_13[0];
+	}
+	if(digital_14[0]==digital_14[1] && digital_14[0]==digital_14[2]&& digital_14[0]==digital_14[3]&& digital_14[0]==digital_14[4])
+	{
+		in_Razmotalka_ReadyServo  = digital_14[0];
+	}
+	if(digital_15[0]==digital_15[1] && digital_15[0]==digital_15[2]&& digital_15[0]==digital_15[3]&& digital_15[0]==digital_15[4])
+	{
+		in_Razmotalka_ReadyNozh = digital_15[0];
+	}
+	if(digital_16[0]==digital_16[1] && digital_16[0]==digital_16[2]&& digital_16[0]==digital_16[3]&& digital_16[0]==digital_16[4])
+	{
+		in_Razmotalka_ReadyDisk = digital_16[0];
+	}
+
 	
 
 }
@@ -182,7 +285,6 @@ void IzhManipulator()
 	This function operates the input manipulator of the system
 */
 {
-	
 	
 	
 	if( ( in_IzhMan_ReperNach && current_speed == 0) ||(in_IzhMan_ReperKrai && current_speed == 0))
